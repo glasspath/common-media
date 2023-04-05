9df0c5e5305c318be5b3576351df5cb68ddef842
remco <remco@192.168.1.70>
2023-04-05 20:36:32 +0200

    Added VideoPreviewPanel

---
+++ common-media-player/src/main/java/org/glasspath/common/media/player/IVideoPreviewPanel.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Glasspath Common.
+ * Copyright (C) 2011 - 2023 Remco Poelstra
+ * Authors: Remco Poelstra
+ * 
+ * This program is offered under a commercial and under the AGPL license.
+ * For commercial licensing, contact us at https://glasspath.org. For AGPL licensing, see below.
+ * 
+ * AGPL licensing:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.glasspath.common.media.player;
+
+import javax.swing.JComponent;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public interface IVideoPreviewPanel {
+
+	public static enum PlayerState {
+		PLAYER_STOPPED,
+		PLAYER_STARTING,
+		PLAYER_STARTED,
+		PLAYER_STOPPING
+	}
+
+	public JComponent getComponent();
+
+	public boolean isPreviewEnabled();
+
+	public void setPreviewEnabled(boolean previewEnabled);
+
+	public void start();
+
+	public void close();
+
+	public JMenu createMenu();
+
+	public JMenu createZoomMenu();
+
+	public JMenuItem createResetViewMenuItem();
+
+}
